eaa27035c017ea97659440a09c4c2eec9720ae59
Beatris <ravagnanibeatris@gmail.com>
2022-03-24 16:34:03 -0300

    Aula 24/03

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,8 +1,11 @@
 public class LivroDeNotas{
     //método de instância
     //instância é sinonimo de objeto 
-    public void exibirMensagem(String mensagem){
-        System.out.println("Bem vindo ao livro de notas!");
+
+    //opera sobre uma instância 
+    String nomeDoCurso;
+    public void exibirMensagem(){
+        System.out.printf("Bem vindo ao livro de notas!", nomeDoCurso);
     }
     
 }
--- Principal.java
+++ Principal.java
@@ -1,10 +1,14 @@
+import static javax.swing.JOptionPane.showInputDialog;
+import static java.lang.Integer.parseInt;
 public class Principal {
     public static void main(String[] args) {
-        //variavel de referencia
-        //é um ponteiro
-        LivroDeNotas LivroDeNotas;
-        LivroDeNotas = new LivroDeNotas();
-        LivroDeNotas.exibirMensagem();
+        String nomeDoCurso = showInputDialog("Digite um curso");
+      LivroDeNotas livro = new LivroDeNotas();
+      livro.nomeDoCurso = nomeDoCurso;
+      livro.exibirMensagem();
+      LivroDeNotas livro2  = new LivroDeNotas();
+      livro2.nomeDoCurso = showInputDialog("Digite um curso");
+      livro2.exibirMensagem();
 
         
     }
