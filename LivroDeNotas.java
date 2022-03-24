0fcb5c80e047d9d48fbb51eb8b914caedaba8fcd
Beatris <ravagnanibeatris@gmail.com>
2022-03-24 16:18:26 -0300

    Aula 24_03

---
+++ LivroDeNotas.java
@@ -0,0 +1,8 @@
+public class LivroDeNotas{
+    //método de instância
+    //instância é sinonimo de objeto 
+    public void exibirMensagem(String mensagem){
+        System.out.println("Bem vindo ao livro de notas!");
+    }
+    
+}
---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        //variavel de referencia
+        //é um ponteiro
+        LivroDeNotas LivroDeNotas;
+        LivroDeNotas = new LivroDeNotas();
+        LivroDeNotas.exibirMensagem();
+
+        
+    }
+}
